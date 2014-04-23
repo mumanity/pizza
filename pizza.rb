0c8ed28b5130681e03e28801b89c6fbd04260243
Emily Schomer <minischomer@gmail.com>
2014-04-23 14:44:38 -0500

    adding .initialize and matching test for Pizza

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,25 @@
 class Pizza
+  attr_accessor :toppings
+
+  def initialize(toppings)
+    @toppings = toppings
+  end
+
 end
 
 class Topping
+  attr_accessor :name, :vegetarian
+
+  # Initialize a new Topping
+  def initialize(name, vegetarian: false)
+    @name = name
+    @vegetarian = vegetarian
+  end
+
+  # name - the String name of the Topping
+
+
+  # returns a new Topping object
+
+
 end
